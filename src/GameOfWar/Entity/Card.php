6bf27dba7a3ff6daa268a4b4f7b2385e93cd2102
Andre Branchizio <andre@Andres-MacBook-Pro.local>
2015-04-21 13:02:04 -0500

    more docs on the entities

--- src/GameOfWar/Entity/Card.php
+++ src/GameOfWar/Entity/Card.php
@@ -8,13 +8,32 @@ use Doctrine\ORM\Mapping AS ORM;
 /**
  * @ORM\Entity(repositoryClass="CardRepository")
  * @ORM\Table(name="cards")
+ *
+ * The entity model for a card in a deck
+ *
+ * @author Andre Jon Branchizio <andrejbranch@gmail.com>
  */
 class Card
 {
+    /**
+     * @var string the club suit type
+     */
     const SUIT_CLUB = "Club";
+
+    /**
+     * @var string the diamond suit type
+     */
     const SUIT_DIAMOND = "Diamond";
+
+    /**
+     * @var string the heart suit type
+     */
     const SUIT_HEART = "Heart";
-    const SUIT_SPADE = "Space";
+
+    /**
+     * @var string the spade suit type
+     */
+    const SUIT_SPADE = "Spade";
 
     /**
      * @var array valid suit values
@@ -29,24 +48,35 @@ class Card
     /**
      * @ORM\Id @ORM\Column(type="integer")
      * @ORM\GeneratedValue
+     * @var int the cards id
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=5)
+     * @var string the name of the card
      */
     private $name;
 
     /**
      * @ORM\Column(type="string", length=8)
+     * @var string the suit type of the card
      */
     private $suit;
 
     /**
      * @ORM\Column(type="integer", length=2)
+     * @var int the relative power of the card
      */
     private $power;
 
+    /**
+     * Initializes a new Card
+     *
+     * @param string $name
+     * @param string $suit
+     * @param int $power
+     */
     public function __construct($name, $suit, $power)
     {
         $this->name = $name;
@@ -54,31 +84,62 @@ class Card
         $this->power = $power;
     }
 
+    /**
+     * Converts the card object to a string value
+     *
+     * @return string
+     */
     public function __tostring()
     {
         return sprintf('%s of %ss', $this->name, $this->suit);
     }
 
+    /**
+     * Set the id of the card
+     *
+     * @param int $id
+     */
     public function setId($id)
     {
         $this->id = $id;
     }
 
+    /**
+     * Get the id of the card
+     *
+     * @return int
+     */
     public function getId()
     {
         return $this->id;
     }
 
+    /**
+     * Set the name of the card
+     *
+     * @param string $name
+     */
     public function setName($name)
     {
         $this->name = $name;
     }
 
+    /**
+     * Get the name of the card
+     *
+     * @return string
+     */
     public function getName()
     {
         return $this->name;
     }
 
+    /**
+     * Set the suit of the card
+     *
+     * @param string $suit
+     * @throws InvalidArgumentException if the provided suit is not in the valid suits array
+     */
     public function setSuit($suit)
     {
         if (!in_array($suit, self::$validSuits)) {
@@ -88,16 +149,31 @@ class Card
         $this->suit = $suit;
     }
 
+    /**
+     * Get the suit of the card
+     *
+     * @return string
+     */
     public function getSuit()
     {
         return $this->suit;
     }
 
+    /**
+     * Set the relative power of the card
+     *
+     * @param int $power
+     */
     public function setPower($power)
     {
         $this->power = $power;
     }
 
+    /**
+     * Get the relative power of the card
+     *
+     * @return int
+     */
     public function getPower()
     {
         return $this->power;
--- src/GameOfWar/Entity/PlayerCard.php
+++ src/GameOfWar/Entity/PlayerCard.php
@@ -8,6 +8,10 @@ use Doctrine\ORM\Mapping AS ORM;
 /**
  * @ORM\Entity()
  * @ORM\Table(name="player_cards")
+ *
+ * The entity model for a player card, this is a linker from player to card
+ *
+ * @author Andre Jon Branchizio <andrejbranch@gmail.com>
  */
 class PlayerCard
 {
@@ -15,6 +19,7 @@ class PlayerCard
      * @ORM\Id
      * @ORM\ManyToOne(targetEntity="GameOfWar\Entity\Player", inversedBy="cards")
      * @ORM\JoinColumn(name="player_id", nullable=false)
+     * @var GameOfWar\Entity\Player the player object that owns the linked card
      */
     protected $player;
 
@@ -22,40 +27,57 @@ class PlayerCard
      * @ORM\Id
      * @ORM\OneToOne(targetEntity="GameOfWar\Entity\Card")
      * @ORM\JoinColumn(name="card_id", nullable=false)
+     * @var GameOfWar\Entity\Card the card object
      */
     protected $card;
 
+    /**
+     * Initializes a new PlayerCard
+     *
+     * @param Player $player
+     * @param Card $card
+     */
     public function __construct(Player $player, Card $card)
     {
         $this->player = $player;
         $this->card = $card;
     }
 
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
-
-    public function getId()
-    {
-        return $this->id;
-    }
-
+    /**
+     * Set the player who owns the card
+     *
+     * @param Player $player
+     */
     public function setPlayer(Player $player)
     {
         $this->player = $player;
     }
 
+    /**
+     * Get the player who owns the card
+     *
+     * @return GameOfWar\Entity\Player
+     */
     public function getPlayer()
     {
         return $this->player;
     }
 
+    /**
+     * Set the card the player owns
+     *
+     * @param Card $card
+     */
     public function setCard(Card $card)
     {
         $this->card = $card;
     }
 
+    /**
+     * Get the card the player owns
+     *
+     * @return GameOfWar\Entity\Card
+     */
     public function getCard()
     {
         return $this->card;
